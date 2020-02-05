9e4a0974e6871c99eec02741442812fba61ebf2c
Kyle Hauskins-Kaplan <Kyle@Kyles-MBP.fios-router.home>
2020-02-05 18:46:14 -0500

    got set up with firebase and vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import { firestorePlugin } from 'vuefire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 Vue.config.productionTip = false
 
+Vue.use(firestorePlugin)
+firebase.initializeApp({
+  projectId: 'service-fusion-project',
+  // databaseURL: 'YOUR_DB_URL'
+})
+export const db = firebase.firestore()
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
